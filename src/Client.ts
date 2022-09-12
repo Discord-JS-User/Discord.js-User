1d06f503e9e60e938f1a1866e931e2bc9538d956
TBHGodPro <tarun.bhaskar@outlook.com>
2022-09-11 21:12:22 -0500

    Undo

--- src/Client.ts
+++ src/Client.ts
@@ -153,6 +153,7 @@ class Client extends (EventEmitter as new () => TypedEmitter<GatewayEvents>) {
 
 		this.socket.onopen = (openEvent: WebSocket.Event) => this.emit("open", openEvent);
 		this.socket.onclose = (closeEvent: WebSocket.CloseEvent) => {
+			if (closeEvent.code == 1) return;
 			this.emit("close", closeEvent);
 			this.reconnect();
 		};
@@ -349,19 +350,6 @@ class Client extends (EventEmitter as new () => TypedEmitter<GatewayEvents>) {
 	 */
 	public async apiFetch(path: string, data?: APIFetchOptions): Promise<any> {
 		return await apiFetch(this.token, path, data);
-	}
-
-	/**
-	 * Close the connection
-	 */
-	public async disconnect() {
-		this.socket.close();
-		this.Logger.log("Disconnecting...");
-		await new Promise(res => setTimeout(res, 3000));
-		if (this.socket.readyState != WebSocket.CLOSED) {
-			this.Logger.log("Took Too Long To Disconnect, Terminating Connection...");
-			this.socket.terminate();
-		}
 	}
 }
 
